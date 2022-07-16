13112c0cd2edf6b0a8805b7f5e30b6c7f916ee8d
Sanjeeb Lama <sanjeeb.lama004@gmail.com>
2022-07-16 17:31:02 +0545

    Change Button color

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("render a button name 'Change color to Blue' ", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonElement = screen.getByRole("button", { name: "Change to Blue" });
+  expect(buttonElement).toHaveStyle({ backgroundColor: "red" });
+});
+
+test("onClick background color of button should be changed to red", () => {
+  render(<App />);
+  const buttonElement = screen.getByRole("button", { name: "Change to Blue" });
+  fireEvent.click(buttonElement);
+
+  expect(buttonElement.textContent).toBe("Change to Red");
+  expect(buttonElement).toHaveStyle({ backgroundColor: "blue" });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [buttonColor, setColor] = useState("red");
+
+  const handleChange = () => {
+    setColor("blue");
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={handleChange} style={{ backgroundColor: buttonColor }}>
+        {buttonColor === "red" ? "Change to Blue" : "Change to Red"}
+      </button>
     </div>
   );
 }
